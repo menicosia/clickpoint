aa8dd568bb8aa8ca528b4a892b1fb03cb9bd69b6
Marco Nicosia <mnicosia@pivotal.io>
2016-01-04 10:14:03 -0800

    - Switched from console.log to util.log, and started using util.format. - Consolidated DB stuff (including schema) into a DB section. - Handler for insert now accepts and send user to a redirect. - Handler for key search now actually reads the results, identifies the URL. - Tables now default active is true, so no need to insert b'1' on each call.

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ var mysql = require('mysql') ;
 
 // Variables
 var port = 8080 ;
-var data = "" ;
 var done = undefined ;
 var db_uri = undefined ;
 var vcap_services = undefined ;
@@ -30,44 +29,48 @@ var dbClient = undefined ;
 var dbConnectState = Boolean(false) ;
 var dbConnectTimer = undefined ;
 
-// Schema definition
-var schema = {
-    redirects : "(id int AUTO_INCREMENT primary key, redirectKey VARCHAR(50), url VARCHAR(2048))",
-    clicks : "(ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, IP VARBINARY(16))"
-} ;
-
 // Setup based on Environment Variables
 if (process.env.VCAP_SERVICES) {
     vcap_services = JSON.parse(process.env.VCAP_SERVICES) ;
     if (vcap_services['p-mysql']) {
         db_uri = vcap_services["p-mysql"][0]["credentials"]["uri"] ;
-        console.log("Got access credentials to p-mysql: " + db_uri) ;
+        util.log("Got access credentials to p-mysql: " + db_uri) ;
     } else if (vcap_services['cleardb']) {
         db_uri = vcap_services["cleardb"][0]["credentials"]["uri"] ;
-        console.log("Got access credentials to ClearDB: " + db_uri) ;
+        util.log("Got access credentials to ClearDB: " + db_uri) ;
     }
 } else {
     db_uri = "mysql://clickpoint:password@127.0.0.1/clickpoint" ;
-    console.log("Local mode, configuring myself to use local MySQL.") ;
+    util.log("Local mode, configuring myself to use local MySQL.") ;
 }
 
 if (process.env.VCAP_APP_PORT) { port = process.env.VCAP_APP_PORT ; }
 
+// DB-related
+
+var schema = {
+    redirects : "(id int AUTO_INCREMENT primary key, redirectKey VARCHAR(50) NOT NULL, url VARCHAR(2048) NOT NULL, active BIT(1) DEFAULT b'1')",
+    clicks : "(ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP, IP VARBINARY(16), value VARCHAR(50), active BIT(1) DEFAULT b'1')"
+} ;
+
 function createOnEmpty(err, results, fields, tableName, create_def) {
+    var sql ;
     if (err) {
         console.error(err) ;
         process.exit(1) ;
     } else {
         if (0 == results.length) {
-            console.log("Creating table: " + tableName) ;
-            dbClient.query(["create table ", tableName, create_def].join(" "),
+            util.log("Creating table: " + tableName) ;
+            sql = util.format("create table %s %s", tableName, create_def) ;
+            console.log(sql) ;
+            dbClient.query(sql,
                            function (err, results, fields) {
                                if (err) {
-                                   console.log("create table error: "
+                                   util.log("create table error: "
                                                + JSON.stringify(err))}
                            } ) ;
         } else {
-            console.log("  [schema] " + tableName + " table already exists.") ;
+            util.log("  [schema] " + tableName + " table already exists.") ;
         }
     }
 }
@@ -85,33 +88,6 @@ function setupSchema() {
     }
 }
     
-// Callback functions
-
-function handleDBerror(err) {
-    if (err) {
-        console.warn("Issue with database, " + err.code + ". Attempting to reconnect every 1 second.")
-        setTimeout(MySQLConnect, 1000) ;
-    }
-}
-
-function handleDBConnect(err) {
-    if (err) {
-        dbConnectState = false ;
-        console.error("ERROR: problem connecting to DB: " + err.code +
-                      ", will try again every 1 second.") ;
-        dbConnectTimer = setTimeout(MySQLConnect, 1000) ;
-    } else {
-        console.log("Connected to database.") ;
-        dbClient.on('error', handleDBerror) ;
-        dbConnectState = true ;
-        if (dbConnectTimer) {
-            clearTimeout(dbConnectTimer) ;
-            dbConnectTimer = undefined ;
-        }
-        setupSchema() ;
-    }
-}
-
 function MySQLConnect() {
     dbClient = mysql.createConnection(db_uri, {debug: true}) ;
     dbClient.connect(handleDBConnect) ;
@@ -131,77 +107,128 @@ function errorDbNotReady(request, response) {
     response.end(errHTML) ;
 }
 
-function handleKeySearch(response, redirectKey, clickArgs) {
+function handleDBerror(err) {
+    if (err) {
+        console.warn("Issue with database, " + err.code
+                     + ". Attempting to reconnect every 1 second.")
+        setTimeout(MySQLConnect, 1000) ;
+    }
+}
+
+function handleDBConnect(err) {
+    if (err) {
+        dbConnectState = false ;
+        console.error("ERROR: problem connecting to DB: " + err.code +
+                      ", will try again every 1 second.") ;
+        dbConnectTimer = setTimeout(MySQLConnect, 1000) ;
+    } else {
+        util.log("Connected to database.") ;
+        dbClient.on('error', handleDBerror) ;
+        dbConnectState = true ;
+        if (dbConnectTimer) {
+            clearTimeout(dbConnectTimer) ;
+            dbConnectTimer = undefined ;
+        }
+        setupSchema() ;
+    }
+}
+
+// Callback functions
+
+function handleClickRecord(response, url) {
     function cb(err, results, fields) {
-        if (1 == results.length) {
-            response.end("Thanks. Check the log.") ;
+        if (err) {
+            util.log("SQL ERR: " + JSON.stringify(err)) ;
+            // Do not error; issue the redirect instead.
+        }
+        console.info(util.format("DB response: %s", JSON.stringify(results))) ;
+        response.writeHead(302, {'Location': url}) ;
+        response.end() ;
+    }
+    return(cb) ;
+}
+
+function handleKeySearch(response, redirectKey, requestIP, value) {
+    function cb(err, results, fields) {
+        var url ;
+        if (err) {
+            response.writeHead(500) ;
+            util.log("SQL ERR: " + JSON.stringify(err)) ;
+            response.end("Internal Server Error: click redirect not found.") ;
+        } else if (1 <= results.length) {
+            url = results[0]["url"] ;
+            sql = util.format("insert into clicks VALUES (%s, '%s', '%s')",
+                              "null", requestIP, value) ;
+            console.info("SQL: " + sql) ;
+            dbClient.query(sql,
+                           handleClickRecord(response, url)) ;
         } else {
+            util.log("Invalid redirectKey request: " + redirectKey) ;
             response.writeHead(404) ;
-            console.log("SQL ERR: " + JSON.stringify(err)) ;
-            response.end("Error: click redirect not found.") ;
+            response.end("The link you've clicked hasn't got an active"
+                         + " redirect associated.") ;
         }
     } ;
     return(cb) ;
 }
 
 // clickArgs are simply the results of url.parse["query"]
-function dispatchApi(response, redirectKey, clickArgs) {
+function dispatchApi(response, redirectKey, requestIP, clickArgs) {
     var sql ;
-    console.log("Request key: " + JSON.stringify(redirectKey),
-                " Query: " + JSON.stringify(clickArgs)) ;
+    var args, value ;
+    
+    args = Object.keys(clickArgs) ;
+    if (0 >= args.length) {
+        console.warn("Invalid redirect arguments: "
+                     + JSON.stringify(clickArgs)) ;
+        response.writeHead(400) ;
+        response.end("The link you clicked on was not properly formed.") ;
+    }
+    value = args[0] ;
+    util.log(util.format("Request key: %s, with argument: %s",
+                         redirectKey, value)) ;
 
-    // Do a lookup in MySQL for the redirect key
-    // Create a record for that redirect key with the IP address and the clickArgs
+    // Do a lookup in MySQL for the redirect key Create a record for
+    // that redirect key with the IP address and the clickArgs
     // Redirect the user to where they're headed
 
-    // Look up the redirect Key
-    sql = "select id,url from redirects WHERE redirectKey = '" + redirectKey + "'" ;
+    sql = util.format("select id,url from redirects WHERE redirectKey = '%s' AND active = b'1'", redirectKey) ;
     console.info("SQL: " + sql) ;
     
     dbClient.query(sql,
-                   handleKeySearch(response, redirectKey, clickArgs)) ;
+                   handleKeySearch(response, redirectKey, requestIP, value)) ;
     
-    // switch (redirectKey) {
-    // case "v25survey":
-    //     data += request.headers["x-forwarded-for"].split(',')[0] ;
-    //     response.writeHead(302, {'Location': "http://www.pivotal.io/"}) ;
-    //     response.end() ;
-    //     break ;
-    // default:
-    //     console.log("Falling through to returning 404") ;
-    //     response.writeHead(404) ;
-    //     response.end("ERROR: Invalid redirect key") ;
-    // }
 }
 
 function requestHandler(request, response, done) {
-    data = "" ;
+    var requestIP ;
     requestParts = url.parse(request.url, true) ;
     requestPath = requestParts["pathname"].split('/') ;
     rootCall = requestParts["pathname"].split('/')[1] ;
-    console.log("Recieved request for: " + rootCall) ;
+    util.log("Recieved request for: " + rootCall) ;
+    if (request.headers["x-forwarded-for"]) {
+        requestIP = request.headers["x-forwarded-for"].split(',')[0] ;
+    } else {
+        // FIXME: This works when deployed to CF only, no local mode.
+        requestIP = "127.0.0.1" ;
+    }
     switch (rootCall) {
     case "info":
-        if (request.headers["x-forwarded-for"]) {
-            data += request.headers["x-forwarded-for"].split(',')[0] ;
-        } else {
-            // FIXME: This works when deployed to CF only, no local mode.
-            data += "127.0.0.1" ;
-        }
-        response.end(data) ;
+        response.end("Your originating IP: " + requestIP) ;
         break ;
     case "click":
         if (3 === requestPath.length) {
             redirectKey = requestParts["pathname"].split('/')[2] ;
-            dispatchApi(response, redirectKey, requestParts["query"]) ;
+            dispatchApi(response, redirectKey,
+                        requestIP, requestParts["query"]) ;
         } else {
             console.warn("Invalid redirect request: " + request.url) ;
-            response.writeHead(404) ;
-            response.end("The link you clicked on was not properly formed, sorry!") ;
+            response.writeHead(400) ;
+            response.end("The link you clicked on was not properly formed") ;
         }
         break ;
     default:
-        console.log("Unhandled request: " + request.url + ", falling through.") ;
+        util.log("Unhandled request: " + request.url + ", falling through.") ;
         done() ;
     }
 }
@@ -218,4 +245,4 @@ clickPointServer = http.createServer(function(req, res) {
 
 clickPointServer.listen(port) ;
 
-console.log("Server up and listening on port: " + port) ;
+util.log("Server up and listening on port: " + port) ;
